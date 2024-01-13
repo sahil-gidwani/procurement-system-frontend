7e4c38dc020cd6a3c3eeebc8327beb71a589c3f8
GitHub Action <action@github.com>
2024-01-13 06:26:58 +0000

    Auto-format code with Prettier

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,27 +1,26 @@
 export default {
-    site: 'https://smart-procurement-system.netlify.app/',
-    scanner: {
-      // run lighthouse for each URL 3 times
-      samples: 3,
-      // use desktop to scan
-      device: 'desktop',
-      // enable the throttling mode
-      throttle: true,
-    },
-    debug: true,
-    // hooks: {
-    //   async authenticate(page) {
-    //     // login to the page
-    //     await page.goto('https://example.com/login')
-    //     const emailInput = await page.$('input[type="email"]')
-    //     await emailInput.type('admin@example.com')
-    //     const passwordInput = await page.$('input[type="password"]')
-    //     await passwordInput.type('password')
-    //     await Promise.all([
-    //       page.$eval('.login-form', form => form.submit()),
-    //       page.waitForNavigation(),
-    //     ])
-    //   },
-    // },
-  }
-  
+  site: "https://smart-procurement-system.netlify.app/",
+  scanner: {
+    // run lighthouse for each URL 3 times
+    samples: 3,
+    // use desktop to scan
+    device: "desktop",
+    // enable the throttling mode
+    throttle: true,
+  },
+  debug: true,
+  // hooks: {
+  //   async authenticate(page) {
+  //     // login to the page
+  //     await page.goto('https://example.com/login')
+  //     const emailInput = await page.$('input[type="email"]')
+  //     await emailInput.type('admin@example.com')
+  //     const passwordInput = await page.$('input[type="password"]')
+  //     await passwordInput.type('password')
+  //     await Promise.all([
+  //       page.$eval('.login-form', form => form.submit()),
+  //       page.waitForNavigation(),
+  //     ])
+  //   },
+  // },
+};
