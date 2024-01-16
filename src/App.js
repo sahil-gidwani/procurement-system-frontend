ed676a043f81b9a8dcafbc5eb2f2e53435c1f2e3
Sahil Gidwani <sahilgdwn@gmail.com>
2024-01-16 17:04:45 +0530

    post css config

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './App.css';
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="heading">Hello world!</h1>;
 }
